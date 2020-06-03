ede799ec88a6360b4a58f01878c890a94dc1f10d
David Horstman <davidzilla27@gmail.com>
2020-06-03 17:49:12 -0400

    Login endpoint skeleton

--- api/userRouter.js
+++ api/userRouter.js
@@ -5,36 +5,63 @@ const { catchAsync, AppError } = require("./errors");
 
 router.post(
   "/register",
-  catchAsync(validateUser),
+  validateUsername("doesn't exist"),
+  validateUserObject,
   catchAsync(async (req, res) => {
-    const { user } = req;
+    const user = req.body;
     user.password = bcrypt.hashSync(user.password, 10);
     const saved = await db.addUser(user);
-    res.status(201).json(saved);
+    res.status(201).json({ ...saved, password: "••••••••••" });
+  })
+);
+
+router.post(
+  "/login",
+  validateUsername("exists"),
+  validateUserObject,
+  catchAsync(async (req, res) => {
+    const { password } = req.body;
+    const { password: passwordHash } = req.user;
+    if (!bcrypt.compareSync(password, passwordHash)) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+    res.status(200).json("Logged in");
   })
 );
 
 /*----------------------------------------------------------------------------*/
 /* Middleware
 /*----------------------------------------------------------------------------*/
-async function validateUser(req, res, next) {
-  const user = req.body;
-  req.user = user;
-  const list = await db.getUsers();
-  const existingWithUsername = list.find(u => u.username == user.username);
-  if (existingWithUsername) {
-    return next(
-      new AppError(`A user with username ${user.username} already exists.`, 403)
-    );
-  }
-  user.username && user.password
+function validateUserObject(req, res, next) {
+  const { username, password } = req.body;
+  username && password
     ? next()
     : next(
         new AppError(
-          "Registration object requires both 'username' and 'password' fields",
+          "User object requires both 'username' and 'password' fields",
           403
         )
       );
+}
+
+function validateUsername(desire) {
+  const userShouldExist = desire === "exists";
+  return catchAsync(async (req, res, next) => {
+    const { username } = req.body;
+    const existingWithUsername = await db.getUser({ username });
+    if (!userShouldExist && existingWithUsername) {
+      return next(
+        new AppError(`A user with username ${username} already exists.`, 403)
+      );
+    } else if (userShouldExist && !existingWithUsername) {
+      return next(
+        new AppError(`No user with username ${username} exists`, 404)
+      );
+    } else {
+      req.user = existingWithUsername;
+      return next();
+    }
+  });
 }
 
 /* Export --------------------------------------------------------------------*/
--- data/userModel.js
+++ data/userModel.js
@@ -3,18 +3,17 @@ const knex = require("./dbConfig");
 function addUser(user) {
   return knex("users")
     .insert(user, ["id"])
-    .then(([id]) => getUser(id));
+    .then(([id]) => getUser({ id }));
 }
 
 function getUsers() {
   return knex("users");
 }
 
-function getUser(id) {
+function getUser(user) {
   return knex("users")
-    .where({ id })
-    .first()
-    .then(user => ({ ...user, password: "••••••••••" }));
+    .where(user)
+    .first();
 }
 
 module.exports = { addUser, getUsers, getUser };
