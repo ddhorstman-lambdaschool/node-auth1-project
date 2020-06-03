ad54dd2e5704a3ccff513e8b3a78e90ae883cefc
David Horstman <davidzilla27@gmail.com>
2020-06-03 17:23:25 -0400

    Add users to database

--- api/userRouter.js
+++ api/userRouter.js
@@ -1,24 +1,40 @@
 const router = require("express").Router();
 const db = require("../data/userModel");
 const bcrypt = require("bcryptjs");
-const { catchAsync } = require("./errors");
+const { catchAsync, AppError } = require("./errors");
 
 router.post(
   "/register",
-  validateUser,
-  catchAsync(async (req, res) => {})
+  catchAsync(validateUser),
+  catchAsync(async (req, res) => {
+    const { user } = req;
+    user.password = bcrypt.hashSync(user.password, 10);
+    const saved = await db.addUser(user);
+    res.status(201).json(saved);
+  })
 );
 
 /*----------------------------------------------------------------------------*/
 /* Middleware
 /*----------------------------------------------------------------------------*/
-function validateUser(req, res, next) {
+async function validateUser(req, res, next) {
   const user = req.body;
+  req.user = user;
+  const list = await db.getUsers();
+  const existingWithUsername = list.find(u => u.username == user.username);
+  if (existingWithUsername)
+    return next(
+      new AppError(`A user with username ${user.username} already exists.`, 403)
+    );
   user.username && user.password
     ? next()
     : next(
-        new AppError("User requires both 'username' and 'password' fields", 404)
+        new AppError(
+          "Registration object requires both 'username' and 'password' fields",
+          403
+        )
       );
 }
 
+/* Export --------------------------------------------------------------------*/
 module.exports = router;
--- data/userModel.js
+++ data/userModel.js
@@ -1,7 +1,14 @@
 const knex = require("./dbConfig");
 
 function addUser(user) {
-  return knex("users").insert(user);
+  return knex("users")
+    .insert(user, ["id"])
+    .then(([id]) =>
+      knex("users")
+        .where({ id })
+        .first()
+        .then(user => ({ ...user, password: "••••••••••" }))
+    );
 }
 
 function getUsers() {
