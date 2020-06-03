099a27df859a3669bcdb36ab2de544cd5d1ff7fe
David Horstman <davidzilla27@gmail.com>
2020-06-03 18:00:24 -0400

    Set up session and split off userRouter from authRouter

---
+++ api/authRouter.js
@@ -0,0 +1,69 @@
+const router = require("express").Router();
+const db = require("../data/userModel");
+const bcrypt = require("bcryptjs");
+const { catchAsync, AppError } = require("./errors");
+
+router.post(
+  "/register",
+  validateUsername("doesn't exist"),
+  validateUserObject,
+  catchAsync(async (req, res) => {
+    const user = req.body;
+    user.password = bcrypt.hashSync(user.password, 10);
+    const saved = await db.addUser(user);
+    res.status(201).json({ ...saved, password: "••••••••••" });
+  })
+);
+
+router.post(
+  "/login",
+  validateUsername("exists"),
+  validateUserObject,
+  catchAsync(async (req, res) => {
+    const { username, password } = req.body;
+    const { password: passwordHash } = req.user;
+    if (!bcrypt.compareSync(password, passwordHash)) {
+      return res.status(401).json({ message: "Invalid password" });
+    }
+    req.session.username = username;
+    res.status(200).json("Logged in");
+  })
+);
+
+/*----------------------------------------------------------------------------*/
+/* Middleware
+/*----------------------------------------------------------------------------*/
+function validateUserObject(req, res, next) {
+  const { username, password } = req.body;
+  username && password
+    ? next()
+    : next(
+        new AppError(
+          "User object requires both 'username' and 'password' fields",
+          403
+        )
+      );
+}
+
+function validateUsername(desire) {
+  const userShouldExist = desire === "exists";
+  return catchAsync(async (req, res, next) => {
+    const { username } = req.body;
+    const existingWithUsername = await db.getUser({ username });
+    if (!userShouldExist && existingWithUsername) {
+      return next(
+        new AppError(`A user with username ${username} already exists.`, 403)
+      );
+    } else if (userShouldExist && !existingWithUsername) {
+      return next(
+        new AppError(`No user with username ${username} exists`, 404)
+      );
+    } else {
+      req.user = existingWithUsername;
+      return next();
+    }
+  });
+}
+
+/* Export --------------------------------------------------------------------*/
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const server = express();
+const helmet = require("helmet");
+const cors = require("cors");
+const session = require("express-session");
+require("dotenv").config();
+const authRouter = require("./authRouter");
 const userRouter = require("./userRouter");
 const { custom404, errorHandling } = require("./errors");
 
+const sessionConfig = {
+  name: "node-auth1-session",
+  secret: process.env.SESSION_SECRET,
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 30,
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+  },
+  resave: false,
+  saveUninitialized: false,
+};
+
+server.use(helmet());
 server.use(express.json());
-server.use("/api", userRouter);
+server.use(cors());
+server.use(session(sessionConfig));
+
+server.use("/api", authRouter);
+server.use("/api/users", userRouter);
 
 /*----------------------------------------------------------------------------*/
 /* Error handling
